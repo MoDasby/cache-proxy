b979022e6a930f934881655fade24fa6d670d219
MoDasby <giullianomendes033@gmail.com>
2025-04-17 15:13:08 -0300

    fix: prevent building null URLs and fix cache clearing endpoint

--- src/main/java/com/modasby/cacheproxy/controller/CacheController.java
+++ src/main/java/com/modasby/cacheproxy/controller/CacheController.java
@@ -1,15 +1,13 @@
 package com.modasby.cacheproxy.controller;
 
 import com.modasby.cacheproxy.proxy.CacheProxy;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/**")
 public class CacheController {
     private final CacheProxy cacheProxy;
 
@@ -17,17 +15,17 @@ public class CacheController {
         this.cacheProxy = cacheProxy;
     }
 
-    @GetMapping
-    public Mono<ResponseEntity<String>> getRequest(HttpServletRequest httpServletRequest) {
-        String path = httpServletRequest.getRequestURI();
-        String queryString = httpServletRequest.getQueryString();
+    @GetMapping("/**")
+    public Mono<ResponseEntity<String>> getRequest(ServerHttpRequest serverHttpRequest) {
+        String path = serverHttpRequest.getURI().getPath();
+        String query = serverHttpRequest.getURI().getQuery();
 
-        String uri = path+"?"+queryString;
+        String uri = query != null ? path + "?" + query : path;
 
         return this.cacheProxy.getData(uri);
     }
 
-    @GetMapping("/clear")
+    @GetMapping("/proxy/clear")
     public ResponseEntity<?> clear() {
         this.cacheProxy.clearCache();
 
